14e8789270ae8fff31830f9d1195494e3197efbc
Dierkalalala <diyorx@mail.ru>
2020-10-08 15:01:25 +0500

    signin form markup is ready to usage

---
+++ static/assets/js/main.js
@@ -0,0 +1,39 @@
+class InputPlaceholder {
+    constructor(inputs) {
+        this.inputs = Array.from(inputs);
+
+        this.inputs.forEach(input => {
+            input.addEventListener('focus', this.focus.bind(input));
+            input.addEventListener('blur', this.blur.bind(input))
+        })
+    }
+    focus(e) {
+        let parent = this.closest('label');
+        parent.classList.add('js-focused');
+    }
+
+    blur(e){
+        let parent = this.closest('label');
+        if(this.value !== '') {
+            return false
+        }
+        parent.classList.remove('js-focused');
+    }
+}
+class formSubmitionHandler{
+    constructor(form) {
+        this.form = form;
+        this.form.addEventListener('submit', this.submit)
+    }
+
+    submit(e) {
+
+        e.preventDefault();
+    }
+
+
+
+}
+
+
+new InputPlaceholder(document.querySelectorAll('.auth-form-wrapper .js-input-control'))
