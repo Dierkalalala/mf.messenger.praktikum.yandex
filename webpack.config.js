9688bc10576c1bab5605e377d2b125824ce311ff
Dierkalalala <diyorx@mail.ru>
2020-12-02 22:51:58 +0500

    project work for 4th spring

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,6 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
           },
           'css-loader',
-          'postcss-loader',
           'sass-loader',
         ],
       },
